1fc1aeb32cce13f0f879432db1ccc1db00cd3e89
Lulama Komana <komanalulama20@gmail.com>
2023-03-29 16:04:23 +0200

    push in javascript fundamentals

---
+++ js_fundamentals/only_the_script.js
@@ -0,0 +1,82 @@
+// var a = 10; 
+
+//var a = 1;
+
+// let a = 10; 
+
+// let s = 10;
+// let b = 2;
+// let c = s % b
+
+// let av = 10;
+
+// let bv = 10;
+
+// let a = 10;
+
+// let b = 20;
+
+// if (av === bv && a == b){
+//     console.log("true")
+// } else {
+//     console.log("false")
+
+// }
+
+// var num1 = 3
+// var num2 = "8" 
+
+// if (num1 === num2){
+//     console.log("false")
+// } else if (num1 != num2){
+//     console.log("true")
+// }
+//swap two variables
+
+// let a = 1
+// let b = 2
+
+// let temp;
+
+// temp = a
+// a = b
+// b = temp
+
+// console.log(b)
+
+// let a = 0;
+
+// if (a > 100) {
+//     console.log("it's not the century")
+// } else if (a === 100) {
+//     console.log("it's a century")
+// } else if (a < 10 && a >= 0) {
+//     console.log("less than a decade")
+// } else {
+//     console.log("Aii nam angazi!!")
+// }
+
+
+var user1 = 100;//lowest
+let user2 = 120;//min
+let user3 = 305;//maximum
+
+if (user1 > user2 && user1 > user3) {
+    console.log(user1)
+}
+else if (user2 > user1 && user2 > user3) {
+    console.log(user2)
+}
+else {
+    console.log(user3)
+}
+
+
+
+
+
+
+
+
+
+

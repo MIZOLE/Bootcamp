93d7756e772dfc30264f324baff73e4eccef82c2
Lulama Komana <komanalulama20@gmail.com>
2023-04-05 15:59:15 +0200

    today's work

--- convert_tempreture/convert.js
+++ convert_tempreture/convert.js
@@ -1,36 +1,44 @@
-//Convert fehrentits  
-function FtoC(degrees) {
+//Convert fehrentits
+function FtoC() {
 
-    let total = (degrees - 32) * 5 / 9
+   let input = document.getElementById("inpu").value
 
-    if (total < 16) {
-        return "p"
+   let btn = document.getElementById("button")
 
-    } else if (total >= 16 && total < 21) {
-        return "l"
+   let F = (input - 32) * 5 / 9
 
-    } else if (total > 21) {
-        return "l"
-    }
-
-}
-console.log(FtoC(2))
-// console.log(FtoC(21) + "" )
-function Cal_Age(born_year) {
-
-   let get_age = 2023 - born_year;
-
-   if (get_age < 0) { //it should not be a negetaive number
-      console.log(get_age)
-   } else if (!get_age) { //if it's not 
-      console.log("Enter your born year")
-   } else if (get_age) {
-      console.log(get_age) //to calculate my age
+   if (F <= 0) {
+      alert("It is colder than the north pole!")
    }
+   else if (F < 16) {
+      alert("It is cold")
+   } else if (F >= 16 && F <= 21) {
+      alert(" it is warm")
+   } else {
+      alert("It is too hot, please wear shorts")
+   }
+
 }
 
-// return year_was_born
-Cal_Age(2024)
+
+
+// FtoC()
+// console.log(""FtoC(21) + "" )
+// function Cal_Age(born_year) {
+
+//    let get_age = 2023 - born_year;
+
+//    if (get_age < 0) { //it should not be a negetaive number
+//       console.log(get_age)
+//    } else if (!get_age) { //if it's not
+//       console.log("Enter your born year")
+//    } else if (get_age) {
+//       console.log(get_age) //to calculate my age
+//    }
+// }
+
+// // return year_was_born
+// Cal_Age(2024)
 //multipply by 5
 //sum the two values = 
 
---
+++ convert_tempreture/sum_number.js
@@ -0,0 +1,8 @@
+
+let myArray = [1, 6, 8, 9, 6, 9, 10]
+let add = 0;
+
+for (let i = 0; i < myArray.length; i++){
+    add += myArray[i]
+}
+console.log(add)
